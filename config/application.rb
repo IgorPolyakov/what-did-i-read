56de956ede47f89753724fbbca69e903c963d624
IgorPolyakov <igorpolyakov@protonmail.com>
2018-04-10 15:37:17 +0700

    rename RambIntro to what-did-i-read

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'action_view/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RambIntro
+module WhatDidIRead
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
