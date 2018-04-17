5ca2476648d99020a0084da7cfce8d56521de653
Alex Bazhenov <bazhenov570@gmail.com>
2018-04-17 22:52:28 +0700

    1st GUI

--- frontend/js/controllers/mainController.js
+++ frontend/js/controllers/mainController.js
@@ -1,4 +1,5 @@
 app.controller('MainController', ['$scope', 'books', function($scope, books){
+	$scope.title = 'Hello'
 	books.success(function(data){
 		$scope.listBooks = data;
 	});
--- frontend/js/services/books.js
+++ frontend/js/services/books.js
@@ -1,3 +1,4 @@
+
 app.factory('books', ['$http', function($http) { 
   return $http.get('http://185.40.31.149:9999/books.json') 
             .success(function(data) { 
