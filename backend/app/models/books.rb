1fc75a17c0be1be45b1a749e4edf8e39d8a6a9df
IgorPolyakov <igorpolyakov@protonmail.com>
2018-04-18 11:00:33 +0700

    add `created` & `updated` to `Book`

--- backend/app/models/books.rb
+++ backend/app/models/books.rb
@@ -6,4 +6,19 @@ class Books
   field :url_cover, type: String
   field :description, type: String
   field :progress, type: Integer
+  field :created_at, type: DateTime, default: Time.zone.now
+  field :updated_at, type: DateTime, default: Time.zone.now
+
+  before_create :set_created_at, :set_updated_at
+  before_save :set_updated_at
+
+  protected
+
+  def set_created_at
+    self.created_at = Time.zone.now
+  end
+
+  def set_updated_at
+    self.updated_at = Time.zone.now
+  end
 end
