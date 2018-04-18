de86676c0729d06ddcffe69bcfcc46bffab2ce27
Alex Bazhenov <bazhenov570@gmail.com>
2018-04-18 13:34:37 +0700

    add new book option

--- frontend/js/controllers/mainController.js
+++ frontend/js/controllers/mainController.js
@@ -1,6 +1,18 @@
-app.controller('MainController', ['$scope', 'books', function($scope, books){
+app.controller('MainController', ['$scope', 'books', '$http', function($scope, books, $http){
 	$scope.title = 'Hello'
 	books.success(function(data){
 		$scope.listBooks = data;
 	});
+	$scope.descMore = false;
+
+	$scope.addBook = function(book){
+		$http.post('http://185.40.31.149/', book)
+		.success(function(result){
+			console.log('Success! yopt')
+		})
+		.error(function(result){
+			console.log('Error blya')
+		});
+	}
+
 }]);
--- frontend/js/services/books.js
+++ frontend/js/services/books.js
@@ -1,4 +1,3 @@
-
 app.factory('books', ['$http', function($http) { 
   return $http.get('http://185.40.31.149:9999/books.json') 
             .success(function(data) { 
