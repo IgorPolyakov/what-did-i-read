16dc7be40f165745d78b20dfc43784777260f549
IgorPolyakov <igorpolyakov@protonmail.com>
2018-04-23 22:47:10 +0700

    Rails 5 API and CORS
    https://til.hashrocket.com/posts/4d7f12b213-rails-5-api-and-cors

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,12 +2,4 @@
 
 class ApplicationController < ActionController::API
   include TokenAuthenticatable
-  after_action :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
 end
