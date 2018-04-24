c8da72c2e8319dfee56fa2b66ae43478b53590d8
IgorPolyakov <igorpolyakov@protonmail.com>
2018-04-24 14:56:18 +0700

    try to fix CORS Preflight Options Request

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('ALLOWED_ORIGINS') { '*' }
+    origins '*'
 
     resource '*',
              headers: :any,
