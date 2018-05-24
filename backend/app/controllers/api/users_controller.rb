486e0dfab41330a4f75a158b838cbedf18442d85
IgorPolyakov <igorpolyakov@protonmail.com>
2018-05-24 12:29:25 +0700

    update #8: removed strange redirect, sing_up api work now

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -20,7 +20,7 @@ module Api
       @user = User.new(user_params)
 
       if @user.save
-        render json: @user, status: :created, location: @user
+        render json: @user, status: :created
       else
         render json: @user.errors, status: :unprocessable_entity
       end
