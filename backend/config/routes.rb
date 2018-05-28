6cd63ff7ca9ce30a73a4ef1621a0e13e8d55b991
IgorPolyakov <igorpolyakov@protonmail.com>
2018-05-28 14:53:25 +0700

    update #12: add api subdomain

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api, path: '/' do
-    resources :auth, only: :create
-    resources :books
-    resources :users
+  constraints subdomain: :api do
+    namespace :api, path: '/' do
+      resources :auth, only: :create
+      resources :books
+      resources :users
+    end
   end
 end
