97cbdba14db4e58e31805ea5abf1ee4a6662d733
IgorPolyakov <igorpolyakov@protonmail.com>
2018-05-29 00:43:18 +0700

    update #12: subdomain not work

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  constraints subdomain: :api do
-    namespace :api, path: '/' do
-      resources :auth, only: :create
-      resources :books
-      resources :users
-    end
+  namespace :api do
+    resources :auth, only: :create
+    resources :books
+    resources :users
   end
 end
