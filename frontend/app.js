0067611d1e9f04e8b297ccf5b9542c270056ec97
AlexBazh <bazhenov570@gmail.com>
2018-05-29 23:24:20 +0700

    small fix

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngMessages', 'ngStorage'])
+        .module('app', ['ngRoute', 'ngStorage'])
         .config(config)
         .run(run);
 
