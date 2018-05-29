b5e75d75e2c4a14467b62ee6fb59579ec90289bd
AlexBazh <bazhenov570@gmail.com>
2018-05-29 23:16:22 +0700

    update #9 new design starting

---
+++ frontend/init.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+var addBook = function(){
+   M.toast({html: 'New Book Added'})
+}
+var editBook = function(){
+   M.toast({html: 'Book Edited'})
+}
+var deleteBook = function(){
+   M.toast({html: 'Book Deleted'})
+}
